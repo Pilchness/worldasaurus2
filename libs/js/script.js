6e46daf6afc1a67d9669ddbf21deb87072db36b1
Pilchness <44652038+Pilchness@users.noreply.github.com>
2020-12-22 15:43:28 +0000

    user country located

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,4 +1,5 @@
 let countryDataArray = [];
+let currentCountryData = {};
 
 //preloader
 $(window).on('load', function () {
@@ -17,8 +18,22 @@ const getUserLatLonCoords = () => {
   }
 };
 
+const drawMapOutlineForCountry = (isoCode) => {
+  layer.clearLayers();
+  countryDataArray.forEach((country) => {
+    if (country.properties.iso_a3 === isoCode) {
+      currentCountryData = country;
+      const countryOutline = layer.addData(country, { style: { color: '#ff0000', weight: 10, opacity: 0.65 } });
+      const countryBounds = countryOutline.getBounds();
+      map.flyToBounds(countryBounds, 14, {
+        animate: true,
+        duration: 3
+      });
+    }
+  });
+};
+
 const identifyCountry = (position) => {
-  console.log(position);
   return $.ajax({
     type: 'POST',
     url: 'libs/php/getPlaceFromLatLong.php',
@@ -26,6 +41,7 @@ const identifyCountry = (position) => {
     data: { latitude: position.coords.latitude, longitude: position.coords.longitude },
     success: function (response) {
       let currentCountryISOCode = response.data.results[0].components['ISO_3166-1_alpha-3'];
+      handleNewCountryChosen(currentCountryISOCode);
       drawMapOutlineForCountry(currentCountryISOCode);
     },
     error: function (errorThrown) {
@@ -42,12 +58,25 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/
   attribution:
     'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
+
+//initiate layers and markers
 const layer = L.geoJSON().addTo(map);
+const markers = L.markerClusterGroup();
+map.addLayer(markers);
+
+const addMarkerGroupToMap = (markerData) => {
+  for (let i = 0; i < markerData.length; i++) {
+    let a = markerData[i];
+    let title = a[2];
+    let marker = L.marker(L.latLng(a[0], a[1]), { title: title });
+    marker.bindPopup(title);
+    markers.addLayer(marker);
+  }
+};
 
 const getCountryDataFromLocalJSON = async () => {
-  let geodata;
   try {
-    geodata = await $.ajax({
+    await $.ajax({
       type: 'POST',
       url: 'libs/php/apicalls.php',
       dataType: 'json',
@@ -69,30 +98,32 @@ const populateSearchElementInNavigationBar = async (countryDataArray) => {
   sortAlphabeticalCountryNames(countryDataArray).forEach((countryDataObject) => {
     let countryName = countryDataObject.properties.name;
     let isoCode = countryDataObject.properties.iso_a3;
-    $('#country-selector').append(`<option value="${isoCode}">${countryName}</option>`);
+    $('#country-selector').append(`<option isoCode="${isoCode}">${countryName}</option>`);
   });
 };
 
 getCountryDataFromLocalJSON();
 
-const drawMapOutlineForCountry = (isoCode) => {
-  layer.clearLayers();
-  countryDataArray.forEach((country) => {
-    if (country.properties.iso_a3 === isoCode) {
-      const countryOutline = layer.addData(country, { style: { color: '#ff0000', weight: 10, opacity: 0.65 } });
+const addCapitalCityPin = () => {
+  console.log(currentCountryData);
+};
 
-      const countryBounds = countryOutline.getBounds();
+const drawMapPinsForCountry = () => {
+  addCapitalCityPin();
+  // addMarkerGroupToMap([
+  //   [-37.8210922667, 175.2209316333, '2'],
+  //   [-37.8210819833, 175.2213903167, '3'],
+  //   [-37.8210881833, 175.2215004833, '3A'],
+  //   [-37.8211946833, 175.2213655333, '1']
+  // ]);
+};
 
-      map.flyToBounds(countryBounds, 14, {
-        animate: true,
-        duration: 3
-      });
-    }
-  });
+const handleNewCountryChosen = (isoCode) => {
+  drawMapOutlineForCountry(isoCode);
+  drawMapPinsForCountry();
 };
 
 //country selector event handler
 $('#country-selector').on('change', function () {
-  console.log(this.value);
-  drawMapOutlineForCountry(this.value);
+  handleNewCountryChosen(this.isoCode);
 });
