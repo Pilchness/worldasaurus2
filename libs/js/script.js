1577106dd696496f72eca3d916d8b484be52ee33
Pilchness <44652038+Pilchness@users.noreply.github.com>
2020-12-22 19:39:23 +0000

    map pin groups functions added

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,59 +1,21 @@
 let countryDataArray = [];
-let currentCountryData = {};
-let currentCountryAdditionalData = {};
 
-//preloader
-$(window).on('load', function () {
-  if ($('#preloader').length) {
-    $('#preloader')
-      .delay(100)
-      .fadeOut('slow', function () {
-        $(this).remove();
-      });
-  }
-});
-
-const getUserLatLonCoords = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(identifyCountry);
-  }
+const currentCountry = {
+  iso_a3: '',
+  iso_a2: '',
+  name: '',
+  region: '',
+  area: 0,
+  borders: [],
+  geoData: {},
+  capitalCity: { lat: 0, lng: 0, name: '' },
+  mapPins: { airports: [{ lat: 0, lng: 0, name: '' }], cities: [] },
+  population: 0,
+  flagUrl: null,
+  photoUrls: [],
+  currency: { name: '', code: '', exchangeRate: 0 },
+  weather: {}
 };
-
-const drawMapOutlineForCountry = (isoCode) => {
-  layer.clearLayers();
-  countryDataArray.forEach((country) => {
-    if (country.properties.iso_a3 === isoCode) {
-      currentCountryData = country;
-      getAdditionalCountryData(isoCode);
-      const countryOutline = layer.addData(country, { style: { color: '#ff0000', weight: 10, opacity: 0.65 } });
-      const countryBounds = countryOutline.getBounds();
-      map.flyToBounds(countryBounds, 14, {
-        animate: true,
-        duration: 3
-      });
-    }
-  });
-};
-
-const identifyCountry = (position) => {
-  return $.ajax({
-    type: 'POST',
-    url: 'libs/php/getPlaceFromLatLong.php',
-    dataType: 'json',
-    data: { latitude: position.coords.latitude, longitude: position.coords.longitude },
-    success: function (response) {
-      console.log(response);
-      let currentCountryISOCode = response.data.results[0].components['ISO_3166-1_alpha-3'];
-      handleNewCountryChosen(currentCountryISOCode);
-      drawMapOutlineForCountry(currentCountryISOCode);
-    },
-    error: function (errorThrown) {
-      console.log(errorThrown);
-    }
-  });
-};
-
-getUserLatLonCoords();
 
 //initiate Leaflet map
 const map = L.map('mapid', { zoomControl: false }).setView([51.505, -0.09], 5);
@@ -77,11 +39,64 @@ const addMarkerGroupToMap = (markerData) => {
   }
 };
 
+//preloader
+$(window).on('load', function () {
+  if ($('#preloader').length) {
+    $('#preloader')
+      .delay(100)
+      .fadeOut('slow', function () {
+        $(this).remove();
+      });
+  }
+});
+
+const getUserLatLonCoords = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(identifyCountry);
+  }
+};
+
+const drawMapOutlineForCountry = (isoCode) => {
+  layer.clearLayers();
+  countryDataArray.forEach((country) => {
+    if (country.properties.iso_a3 === isoCode) {
+      currentCountry.geoData = country;
+      currentCountry.iso_a2 = country.properties.iso_a2;
+      currentCountry.iso_a3 = isoCode;
+      getAdditionalCountryData(isoCode);
+      const countryOutline = layer.addData(country, { style: { color: '#ff0000', weight: 10, opacity: 0.65 } });
+      const countryBounds = countryOutline.getBounds();
+      map.flyToBounds(countryBounds, 14, {
+        animate: true,
+        duration: 3
+      });
+    }
+  });
+};
+
+const identifyCountry = (position) => {
+  return $.ajax({
+    type: 'POST',
+    url: 'libs/php/getPlaceFromLatLong.php',
+    dataType: 'json',
+    data: { latitude: position.coords.latitude, longitude: position.coords.longitude },
+    success: function (response) {
+      let currentCountryISOCode = response.data.results[0].components['ISO_3166-1_alpha-3'];
+      handleNewCountryChosen(currentCountryISOCode);
+    },
+    error: function (errorThrown) {
+      console.log(errorThrown);
+    }
+  });
+};
+
+getUserLatLonCoords();
+
 const getCountryDataFromLocalJSON = async () => {
   try {
     await $.ajax({
       type: 'POST',
-      url: 'libs/php/apicalls.php',
+      url: 'libs/php/localGeodata.php',
       dataType: 'json',
       data: { action: 'all' }
     }).then((countryObjectArray) => {
@@ -96,12 +111,22 @@ const getCountryDataFromLocalJSON = async () => {
 const getAdditionalCountryData = (ISOcode) => {
   $.ajax({
     type: 'POST',
-    url: 'libs/php/getCountryFlagData.php',
+    url: 'libs/php/restCountries.php',
     dataType: 'json',
     data: { code: ISOcode },
     success: function (response) {
-      currentCountryAdditionalData = response.data;
-      console.log(currentCountryAdditionalData);
+      currentCountry.name = response.data.name;
+      currentCountry.borders = response.data.borders;
+      currentCountry.currency.name = response.data.currencies[0].code;
+      currentCountry.flagUrl = response.data.flag;
+      currentCountry.population = response.data.population;
+      currentCountry.region = response.data.region;
+      currentCountry.area = response.data.area;
+      currentCountry.capitalCity = response.data.capital;
+
+      console.log(currentCountry);
+
+      drawMapPinsForCountry();
     },
     error: function (errorThrown) {
       console.log(errorThrown);
@@ -123,24 +148,60 @@ const populateSearchElementInNavigationBar = async (countryDataArray) => {
 
 getCountryDataFromLocalJSON();
 
-const addCapitalCityPin = () => {
-  console.log(currentCountryData);
+const addCityPin = (cityName) => {
+  $.ajax({
+    url: 'libs/php/getLatLongFromPlacename.php',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      city: cityName.replace(/ /g, '%20')
+    },
+
+    success: function (result) {
+      let coords = result.data.results[0].geometry;
+      currentCountry.mapPins.cities.push({ lat: coords.lat, lng: coords.lng, name: cityName });
+      addMarkerGroupToMap([[coords.lat, coords.lng, cityName]]);
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
 };
 
-const drawMapPinsForCountry = () => {
-  addCapitalCityPin();
-  // addMarkerGroupToMap([
-  //   [-37.8210922667, 175.2209316333, '2'],
-  //   [-37.8210819833, 175.2213903167, '3'],
-  //   [-37.8210881833, 175.2215004833, '3A'],
-  //   [-37.8211946833, 175.2213655333, '1']
-  // ]);
+const getCityData = async () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: 'libs/php/geoNames.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        query: 'cities',
+        isoA2: currentCountry.iso_a2
+      },
+      success: function (result) {
+        resolve(result);
+      },
+      error: function (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
+const drawMapPinsForCountry = async () => {
+  addCityPin(currentCountry.capitalCity);
+  getCityData().then((cities) => {
+    currentCountry.mapPins.cities = cities.data.geonames;
+    addMarkerGroupToMap(
+      currentCountry.mapPins.cities.map((city) => {
+        return [city.lat, city.lng, city.name];
+      })
+    );
+  });
 };
 
 const handleNewCountryChosen = (isoCode) => {
-  console.log(isoCode);
   drawMapOutlineForCountry(isoCode);
-  drawMapPinsForCountry();
 };
 
 //country selector event handler
